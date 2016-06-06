db23a5140a3a0ea61681a3f65007fa33f22fc780
darren <764190362@qq.com>
2016-06-06 16:43:27 +0800

    finish04

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyArNuJr2cyoM6E2ApUBeLIMTYCqAIcXZeY';
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
